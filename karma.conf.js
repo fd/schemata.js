44435fb94aa57437e66c06fb51c4ecf060231fac
Simon Menke <simon.menke@gmail.com>
2015-03-06 17:25:21 +0100

    Added wecker file & fixed karma config

--- karma.conf.js
+++ karma.conf.js
@@ -69,12 +69,6 @@ module.exports = function(config) {
     },
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
     // web server port
     port: 9876,
 
@@ -86,19 +80,5 @@ module.exports = function(config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    // autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Chrome', 'Firefox', 'Safari'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    // singleRun: false
   });
 };
