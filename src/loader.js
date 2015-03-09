cf6f0082e1f982e47d05ec68dc49957a859ac844
Simon Menke <simon.menke@gmail.com>
2015-03-09 11:36:01 +0100

    Fixed fetch

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,7 @@
 import {typeOf, isSchema, normalizeId} from './util';
 import {build, isMetaSchema, getMetaSchema, determineMetaSchema} from './builder';
 import {Schema, $id} from './schema';
+import 'polyfill/experimental/fetch';
 
 if (!window.fetch) {
   throw Error("please include a fetch() polyfill");
