2b6ad5beee42ec98002d52a48161d5db772655a5
Simon Menke <simon.menke@gmail.com>
2015-03-09 10:01:30 +0100

    WIP CI tests

--- test.js
+++ test.js
@@ -1,7 +1,67 @@
-var proc = require('child_process');
+var spawn = require('child_process').spawn;
 
-process.env.BATCH_ID = 1;
-proc.execSync('node_modules/karma/bin/karma start');
+var batches = require('./test_browsers.js');
 
-process.env.BATCH_ID = 2;
-proc.execSync('node_modules/karma/bin/karma start');
+jspmInstall(function(err, code) {
+  assertOK(err, code);
+  runBatches(batches.length);
+});
+
+function jspmInstall(clb) {
+  var ps = spawn('jspm', ['install']);
+  handleChildProcess(ps, clb);
+}
+
+function runBatches(n, i) {
+  if (i === undefined) {
+    i = 0;
+  }
+
+  if (i >= n) return;
+
+  runBatch(i+1, function(err, code) {
+    assertOK(err, code);
+    runBatches(n, i+1);
+  });
+}
+
+function runBatch(batch_id, clb) {
+  process.env.BATCH_ID = batch_id;
+  var ps = spawn('node_modules/karma/bin/karma', ['start']);
+  handleChildProcess(ps, clb);
+}
+
+function assertOK(err, code) {
+  if (err) {
+    console.log("error: "+err);
+  }
+  if (code !== 0) {
+    process.exit(code);
+  }
+}
+
+function handleChildProcess(ps, clb) {
+  var done = false;
+
+  ps.on('error', function (err) {
+    if (!done) {
+      done = true;
+      clb(err, 1);
+    }
+  });
+
+  ps.stdout.on('data', function (data) {
+    process.stdout.write(data);
+  });
+
+  ps.stderr.on('data', function (data) {
+    process.stderr.write(data);
+  });
+
+  ps.on('close', function (code) {
+    if (!done) {
+      done = true;
+      clb(null, code);
+    }
+  });
+}
---
+++ test_browsers.js
@@ -0,0 +1,87 @@
+modules.exports = [
+  {
+    sl_chrome35: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '35'
+    },
+    sl_chrome36: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '36'
+    },
+    sl_chrome37: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '37'
+    }
+  },
+  {
+    sl_chrome38: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '38'
+    },
+    sl_chrome39: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '39'
+    },
+    sl_chrome40: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '40'
+    }
+  },
+  {
+    sl_firefox30: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_firefox31: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '31'
+    },
+    sl_firefox32: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '32'
+    }
+  },
+  {
+    sl_firefox33: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '33'
+    },
+    sl_firefox34: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '34'
+    },
+    sl_firefox35: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '35'
+    }
+  },
+  {
+    sl_ie9: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '9'
+    },
+    sl_ie10: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '10'
+    },
+    sl_ie11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '11'
+    }
+  }
+];
