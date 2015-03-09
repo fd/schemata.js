91b9686a9b759a665efcde236489e55e888a5574
Simon Menke <simon.menke@gmail.com>
2015-03-09 08:56:42 +0100

    updated config

--- karma.conf.js
+++ karma.conf.js
@@ -83,7 +83,6 @@ module.exports = function(config) {
   config.set({
     sauceLabs: {
       testName: 'Schemata.js',
-      build:    process.env.WERCKER_BUILD_ID,
       recordVideo:       false,
       recordScreenshots: false,
       captureHtml:       false,
